c2e8acc74348116f1018405e320cf5cee861f320
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-02-16 17:56:21 +0000

    i don't know

--- client/static/js/app.js
+++ client/static/js/app.js
@@ -1,5 +1,83 @@
+
 const form = document.querySelector('.telegraph');
+const publishBtn = document.querySelector('#publish')
 form.addEventListener('submit', submitPost);
+
+
+// form.addEventListener('submit', sendPost);
+// window.addEventListener('hashchange', updateContent);
+// window.addEventListener('load', updateContent);
+
+
+// async function getPost(id) {
+//     try {
+//         const response = await fetch(`http://localhost:3000/posts/${id}`);
+//         const data = await response.json();
+//         return data;
+//     } catch (err) {
+//         console.warn(err);
+//     }
+// }
+
+// async function getNewPost(id) {
+//     const postData = await getPost(id)
+//     return postData
+// }
+
+// function updateContent() {
+//     const id = window.location.hash.substring(1)
+//     id.length > 0 ? renderPost(id) : renderForm()
+// }
+
+// async function renderPost(id){
+//     const postList = document.getElementById('postList')
+//     const postData = await getNewPost(id)
+//     postList.innerHTML = `
+//     <div class="post-cont">
+//     <h2>${postData.title}</h2>
+//     <h3 class="post" id="name">${postData.name}</h3><span> ● ${postData.date}</span>
+//     <p>${postData.entry}<p>
+//     </div>`
+// }
+
+// async function sendPost(e){
+//     e.preventDefault();
+//     try {
+//         const options = {
+//             method: 'POST',
+//             headers: { "Content-Type": "application/json" },
+//             body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
+//         }
+        
+//         const response = await fetch('http://localhost:3000/posts', options);
+//         const data = await response.json();
+//         window.location.hash = `#${data}`
+//     } catch (err) {
+//         console.warn(err);
+//     }
+// }
+
+// function renderForm(){
+//     const fields = [
+//         { tag: 'input', attributes: {autocomplete : "off", id: "title", required: "true", type: 'text', name: 'title', placeholder: 'Title' } },
+//         { tag: 'input', attributes: {autocomplete : "off", id: "name", type: 'text', name: 'pseudonym', placeholder: 'Ninja name...' } },
+//         { tag: 'textarea', attributes: { name: 'content', required: "true", placeholder: 'Ninja post...' } },
+//         { tag: 'input', attributes: { type: 'submit', value: 'submit' } }
+//     ]
+//     const postList = document.getElementById('postList')
+//     // const form = document.createElement('form');
+//     //     fields.forEach(f => {
+//     //         const field = document.createElement(f.tag);
+//     //         field.textContent = f.text;
+//     //         Object.entries(f.attributes).forEach(([a, v]) => field.setAttribute(a, v))
+//     //         form.appendChild(field);
+//     //     })
+//         form.onsubmit = sendPost;
+//         postList.appendChild(form);
+//     }
+
+
+
 
 
 getAllPosts();
@@ -11,28 +89,44 @@ function getAllPosts(){
         .catch(console.warn)
 };
 
-// create
-function submitPost(e) {
-    e.preventDefault();
+async function getPost(id) {
+    try {
+        const response = await fetch(`http://localhost:3000/posts/${id}`);
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.warn(err);
+    }
+}
 
-    const postData = {
+// create
+async function submitPost(e) {
+    e.preventDefault();
+    
+    let postData = {
         title: e.target.title.value,
         name: e.target.name.value,
         date: e.target.name.value,
         entry: e.target.name.value
     };
 
-    const options = { 
-        method: 'POST',
-        body: JSON.stringify(postData),
-        headers: { "Content-Type": "application/json" }
-    };
+    try {
 
-    fetch('http://localhost:3000/', options)
-        .then(r => r.json())
-        .then(appendPost)
-        .then(() => e.target.reset())
-        .catch(console.warn)
+        const options = { 
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: { "Content-Type": "application/json" }
+        };
+
+        const response = await fetch('http://localhost:3000/', options)
+        const data = await response.json()
+        window.location.hash = `#${data}`
+        e.target.reset()
+
+    } catch(err) {
+        console.warn(err)
+    }
+        
 };
 
 // helpers
@@ -71,6 +165,5 @@ function appendPost(postData) {
     postDiv.append(entryDiv);
 
     postList.append(postDiv)
-
-
+    
 };
--- server/api/controllers/posts.js
+++ server/api/controllers/posts.js
@@ -24,9 +24,9 @@ router.get('/:id', async (req, res) => {
 })
 
 // Create post route
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     try {
-        const post = Post.create(req.body.title, req.body.name, req.body.date, req.body.entry)
+        const post = await Post.create(req.body.title, req.body.name, req.body.date, req.body.entry)
         res.json(post)
     } catch(err) {
         res.status(404).json({err})
