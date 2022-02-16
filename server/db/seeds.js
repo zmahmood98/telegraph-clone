b1f0389fb6cfb497cb23d1826d9e2332dd7a8a93
saminakhan999 <Samina.khan9999@gmail.com>
2022-02-16 10:10:15 +0000

    posts

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -3,9 +3,9 @@ const db = connect("mongodb://localhost:27017/shelter")
 db.posts.drop()
 
 db.posts.insertMany([
-    { title: "Not a Noob", name: 'Zxhrxm' , date: 16/02/2022 , entry: "antimatter" },
-    { title: "I Am A Giant Noob", name: 'MegaTinySK' , date: 16/02/2022 , entry: "Zahra is so much better than me at wordle, quordle and nerdle" },
-    { title: "Clowns", name: 'MegaTinySK' , date: 16/02/2022 , entry: "Family 'creeped out and scared' after sinister clown painting appears in garden no one knows where the painting came from or who did it (it wasn't me)." }
+    { title: "Not a Noob", name: 'MegaTinySK' , date: 16/02/2022 , entry: "antimatter" },
+    { title: "I Am A Giant Noob", name: 'Zxhrxm' , date: 16/02/2022 , entry: "Zahra is so much better than me at wordle, quordle and nerdle" },
+    { title: "Clowns", name: 'Zxhrxm' , date: 16/02/2022 , entry: "Family 'creeped out and scared' after sinister clown painting appears in garden no one knows where the painting came from or who did it (it wasn't me)." }
 ])
 
 
