fe806b81a2195925d63723615ab8adcdcb6623f8
Samina Khan <67604723+saminakhan999@users.noreply.github.com>
2022-02-17 01:14:45 +0000

    added date function

--- client/static/js/layout.js
+++ client/static/js/layout.js
@@ -1,13 +1,62 @@
+const { sendPost, getPost } = require('./api.js')
+const { encode } = require('html-entities');
 
-const titleTextArea = document.getElementById('titleTextArea')
-
-titleTextArea.addEventListener('click', () => {
-    document.getElementById('titleLabel').style.display = 'inline'
-})
+window.addEventListener('hashchange', updateContent);
+window.addEventListener('load', updateContent);
 
 
-const yourNameTextArea = document.getElementById('yourNameTextArea')
+function updateContent() {
+    const postArea = document.getElementById('postArea')
+    postArea.innerHTML = ''
+    const id = window.location.hash.substring(1)
+    id.length > 0 ? renderPost(id) : renderForm()
+}
 
-yourNameTextArea.addEventListener('click', () => {
-    document.getElementById('yourNameLabel').style.display = 'inline'
-})
+async function renderPost(id){
+    const postArea = document.getElementById('postArea')
+    const postData = await getNewPost(id)
+    postArea.innerHTML = `
+    <div class="post-cont">
+    <h2>${encode(postData.title)}</h2>
+    <h3 class="post" id="entry">${encode(postData.entry)}<h3>
+    <p id="name">${encode(postData.username)}</p>
+    <span id="date">  ${prettyDate(postData.date)}</span>
+    </div>`
+}
+
+function prettyDate(date) {
+    const jsDate = new Date(Date.parse(date))
+    const day = jsDate.getDate()	
+    const month = jsDate.getMonth() + 1
+    const year = jsDate.getFullYear()
+    return `${day}-${month}-${year}`
+}
+
+async function getNewPost(id){
+    const postData = await getPost(id)
+    return postData
+}
+
+function renderForm(){
+    const fields = [
+        { tag: 'input', attributes: {autocomplete : "off", id: "title", required: "true", type: 'text', name: 'title', placeholder: 'Title' } },
+        { tag: 'input', attributes: {autocomplete : "off", id: "name", type: 'text', name: 'username', placeholder: 'Username' } },
+        { tag: 'textarea', attributes: { name: 'entry', required: "true", placeholder: 'Entry' } },
+        { tag: 'input', attributes: { type: 'submit', value: 'submit' } }
+    ]
+    const postArea = document.getElementById('postArea')
+    const form = document.createElement('form');
+        fields.forEach(f => {
+            const field = document.createElement(f.tag);
+            field.textContent = f.text;
+            Object.entries(f.attributes).forEach(([a, v]) => field.setAttribute(a, v))
+            form.appendChild(field);
+        })
+        form.onsubmit = sendPost;
+        postArea.appendChild(form);
+    }
+
+module.exports = {
+    renderPost, renderForm, updateContent
+}
+    
